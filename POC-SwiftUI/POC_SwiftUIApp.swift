59ef55a7c6dc2c4a22ae9ef589919d589831b8b8
User <khalil.mhelheli@gmail.com>
2024-07-07 13:47:40 +0200

    first commit

---
+++ POC-SwiftUI/POC_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_SwiftUIApp.swift
+//  POC-SwiftUI
+//
+//  Created by User on 07/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct POC_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
